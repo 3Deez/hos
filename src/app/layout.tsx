d990b266cc7a6d70163db04819ca1deb15afd016
Muhammad Lawal Adamu <lawaldd@gmail.com>
2023-05-12 14:43:38 +0100

    minor updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,30 @@
-import Navbar from "@/components/Navbar/Navbar";
-import "./globals.css";
-import { Inter, Poppins } from "next/font/google";
+import Navbar from '@/components/Navbar/Navbar'
+import './globals.css'
+import { Inter, Poppins } from 'next/font/google'
 
 const poppins = Poppins({
-  weight: ["800", "300", "700", "600", "500", "900"],
-  style: "normal",
-  subsets: ["latin"],
-});
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  style: 'normal',
+  subsets: ['latin'],
+})
 
 export const metadata = {
-  title: "Niger State Head of Service",
-  description: "Created by Lawal Adamu & Hassan Yakubu",
-};
+  title: 'Niger State Head of Service',
+  description: 'Created by Lawal Adamu & Hassan Yakubu',
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={poppins.className}>
         <Navbar />
 
-        <div className='bg-red-100'>{children}</div>
+        <div className="bg-red-100">{children}</div>
       </body>
     </html>
-  );
+  )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,25 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link'
+import React from 'react'
 
 function Navbar() {
   return (
-    <div className='flex items-center justify-center w-full text-2xl font-bold text-white border'>
-      <div className='fixed w-11/12 py-6 px-8 bg-primary flex items-center justify-between rounded-md top-8 h-[100px]'>
-        <div className='w-3/12'>
-          <Link href='/'>HOS, NIGER STATE</Link>
+    <div className="flex items-center justify-center w-full text-lg font-semibold text-white border">
+      <div className="fixed w-11/12 py-6 px-8 bg-primary flex items-center justify-between rounded-md top-8">
+        <div className="w-3/12">
+          <Link href="/">HOS, NIGER STATE</Link>
         </div>
-        <div className='flex items-center justify-center w-5/12 gap-x-7'>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About</Link>
-          <Link href='/media'>Media</Link>
-          <Link href='/mda'>MDA</Link>
+        <div className="flex items-center justify-center w-5/12 gap-x-7">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/media">Media</Link>
+          <Link href="/mda">MDA</Link>
         </div>
-        <div className='w-3/12 text-right'>
+        <div className="w-3/12 text-right">
           <p>Contact</p>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
