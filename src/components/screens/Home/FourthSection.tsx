df28acb99893420d223625db8fa1fea9b4cc3a8d
yarcx <yakubuh56@gmail.com>
2023-05-20 12:52:22 +0100

    update on the fourth section

--- src/components/screens/Home/FourthSection.tsx
+++ src/components/screens/Home/FourthSection.tsx
@@ -4,16 +4,16 @@ import Image from 'next/image'
 
 const FourthSection = () => {
   return (
-    <div
-      className='flex h-screen items-center justify-center'
+    <section
+      className='flex min-h-screen items-center justify-center'
       style={{
         background: "linear-gradient(180deg, rgba(30, 152, 201, 0.8) 0%, #1E98C9 120%), url('/images/home-bg.png')",
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <div className='mx-auto flex w-full items-start justify-center'>
-        <div className='flex h-[550px] w-4/12 items-center justify-center'>
-          <div className='flex h-full w-[420px] flex-col items-start justify-end overflow-hidden rounded-xl'>
+      <main className='mx-auto flex w-full flex-col items-start justify-center md:flex-row'>
+        <div className='flex h-[550px] items-center justify-center md:w-4/12'>
+          <div className='flex h-full w-[300px] flex-col items-start justify-end overflow-hidden rounded-xl md:w-[420px]'>
             <div className='relative h-[70%] w-full'>
               <Image
                 src='/images/hos.jpg'
@@ -32,7 +32,7 @@ const FourthSection = () => {
           </div>
         </div>
 
-        <div className='flex h-full w-6/12 flex-col rounded-xl pl-16 pt-5'>
+        <div className='flex h-full flex-col rounded-xl pl-16 pt-5 md:w-6/12'>
           <h1 className='text-[2.8rem] font-bold text-white'>Head of Service Niger State</h1>
 
           <p className='mt-16 w-10/12 text-xl text-white'>
@@ -48,8 +48,8 @@ const FourthSection = () => {
             </Link>
           </button>
         </div>
-      </div>
-    </div>
+      </main>
+    </section>
   )
 }
 
